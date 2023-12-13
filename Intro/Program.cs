d03ba056ff892e581406edb7231b9118ebf5715f
Black-Horse <gaspard.meritus@gmail.com>
2023-12-12 20:59:44 -0600

    Add project files.

---
+++ Intro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro
+{
+    public class Person
+    {
+        public string Name;
+        public void Introduce() { }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
